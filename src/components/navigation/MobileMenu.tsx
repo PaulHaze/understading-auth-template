1b75f51f587f5adc39e27ad4c24af949ea22fd95
PaulHaze <dev@artemist.com.au>
2024-03-05 12:46:19 +1100

    feat: add basic links to mobile menu

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -1,14 +1,33 @@
 'use client';
 
-import Hamburger from 'hamburger-react';
+import { useState, useRef } from 'react';
+import Link from 'next/link';
+import { useClickAway } from 'react-use';
 
-import { useState } from 'react';
+// import { AnimatePresence, motion } from 'framer-motion';
+
+import Hamburger from 'hamburger-react';
 
 export function MobileMenu() {
   const [isOpen, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway(ref, () => setOpen(false));
   return (
-    <div className="sm:hidden">
+    <div ref={ref} className="sm:hidden">
       <Hamburger toggled={isOpen} toggle={setOpen} size={22} />
+      {isOpen && (
+        <div className="fixed left-0 right-0 top-[3.5rem] border-b border-b-white/20 bg-[#05070C] p-5 pt-0 shadow-2xl">
+          <ul className="grid gap-2">
+            <li className="w-full rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 p-[0.08rem]">
+              <Link href="/protected-one">Page one</Link>
+            </li>
+            <li className="w-full rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 p-[0.08rem]">
+              <Link href="/protected-two">Page two</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
